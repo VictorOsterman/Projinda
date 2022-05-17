d6a3fa0d233e8a9fb7bf80b174426dd4af719ecf
Victor Österman <vios@kth.se>
2022-05-17 16:32:07 +0200

    Add background to map

---
+++ assets/maps/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="background" tilewidth="64" tileheight="64" tilecount="700" columns="35">
+ <image source="background.png" width="2280" height="1280"/>
+</tileset>
